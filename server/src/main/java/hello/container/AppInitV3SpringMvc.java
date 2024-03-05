2e363c86cbb9c8a9653d5faec5edd9f93a5a37c0
Royi Kim <hellowol@naver.com>
2024-03-05 23:52:47 +0900

    [섹션2 웹 서버와 서블릿 컨테이너] 스프링 MVC 서블릿 컨테이너 초기화 지원

--- server/src/main/java/hello/container/AppInitV3SpringMvc.java
+++ server/src/main/java/hello/container/AppInitV3SpringMvc.java
@@ -2,14 +2,16 @@ package hello.container;
 
 import hello.spring.HelloConfig;
 import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-public class AppInitV2Spring implements AppInit {
+public class AppInitV3SpringMvc implements WebApplicationInitializer {
     @Override
-    public void onStartup(ServletContext servletContext) {
-        System.out.println("AppInitV2Spring.onStartup");
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        System.out.println("AppInitV3SpringMvc.onStartup");
 
         //스프링 컨테이너 생성
         AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
@@ -19,9 +21,9 @@ public class AppInitV2Spring implements AppInit {
         DispatcherServlet dispatcher = new DispatcherServlet(appContext);
 
         //디스패처 서블릿을 서블릿 컨테이너에 등록 (이름 주의! dispatcherV2)
-        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcherV2", dispatcher);
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcherV3", dispatcher);
 
         // /spring/* 요청이 디스패처 서블릿을 통하도록 설정
-        servlet.addMapping("/spring/*");
+        servlet.addMapping("/");
     }
 }
