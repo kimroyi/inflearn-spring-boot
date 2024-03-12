a58c076ab8404757d76cf375d444bb7cfafdd174
Royi Kim <hellowol@naver.com>
2024-03-12 23:32:07 +0900

    [섹션5 자동 구성(Auto Configuration)] @Conditional

--- autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,11 +1,15 @@
 package hello.config;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+// if문과 유사, MemoryCondition의 실행결과가 true이면 빈 등록 그렇지 않으면 빈등록을 안한다.
+@Conditional(MemoryCondition.class)
 public class MemoryConfig {
 
     @Bean
