83f2e75aa94146273e1356a3590373b851d28cae
Royi Kim <hellowol@naver.com>
2024-03-25 01:21:59 +0900

    [섹션7 외부설정과 프로필2] @Profile

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -12,12 +12,12 @@ import org.springframework.context.annotation.Import;
 //@Import(MyDataSourceConfigV1.class)
 //@Import(MyDataSourceConfigV2.class)
 @Import(MyDataSourceConfigV3.class)
-@SpringBootApplication(scanBasePackages = "hello.datasource")
+@SpringBootApplication(scanBasePackages = {"hello.datasource", "hello.pay"})
 /**
  * `@ConfigurationProperties`를 특정 범위로 자동 등록할 때는 `@ConfigurationPropertiesScan`를 사용한다.
  * 빈은 직접 등록하는 것과 컴포넌트 스캔을 사용하는 차이와 비슷하다.
+ * @ConfigurationPropertiesScan
  */
-@ConfigurationPropertiesScan
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
